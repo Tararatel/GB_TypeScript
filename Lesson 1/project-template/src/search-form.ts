b744d1e1cd9dda491e2cb7905372e0d778b3765a
Tararatel <alekseysidorov.28@gmail.com>
2021-12-27 13:18:25 +0300

    Lesson 1 is completed

---
+++ Lesson 1/project-template/src/search-form.ts
@@ -0,0 +1,61 @@
+import { renderBlock } from "./lib.js";
+
+export function renderSearchFormBlock(arrivalDate, departureDate) {
+  const date = new Date();
+  const minDate = new Date().toISOString().split("T")[0];
+  const maxDate = new Date(date.getFullYear(), date.getMonth() + 2, 0)
+    .toISOString()
+    .split("T")[0];
+  const nextDay = (arrivalDate = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate() + 2
+  )
+    .toISOString()
+    .split("T")[0]);
+  const departuretDay = (departureDate = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate() + 4
+  )
+    .toISOString()
+    .split("T")[0]);
+
+  renderBlock(
+    "search-form-block",
+    `
+    <form>
+      <fieldset class="search-filedset">
+        <div class="row">
+          <div>
+            <label for="city">Город</label>
+            <input id="city" type="text" disabled value="Санкт-Петербург" />
+            <input type="hidden" disabled value="59.9386,30.3141" />
+          </div>
+          <!--<div class="providers">
+            <label><input type="checkbox" name="provider" value="homy" checked /> Homy</label>
+            <label><input type="checkbox" name="provider" value="flat-rent" checked /> FlatRent</label>
+          </div>--!>
+        </div>
+        <div class="row">
+          <div>
+            <label for="check-in-date">Дата заезда</label>
+            <input id="check-in-date" type="date" value="${arrivalDate}" min="${minDate}" max="${maxDate}" name="checkin" />
+          </div>
+          <div>
+            <label for="check-out-date">Дата выезда</label>
+            <input id="check-out-date" type="date" value="${departureDate}" min="${nextDay}" max="${maxDate}" name="checkout" />
+          </div>
+          <div>
+            <label for="max-price">Макс. цена суток</label>
+            <input id="max-price" type="text" value="" name="price" class="max-price" />
+          </div>
+          <div>
+            <div><button>Найти</button></div>
+          </div>
+        </div>
+      </fieldset>
+    </form>
+    `
+  );
+}
