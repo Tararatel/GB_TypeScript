54b25c28ef6f99e342d81c6890ae1f3733c6fbef
Tararatel <alekseysidorov.28@gmail.com>
2021-12-29 17:29:00 +0300

    Lesson 2 is comleted

---
+++ Lesson 2/project-template/src/user.ts
@@ -0,0 +1,30 @@
+import { renderBlock } from "./lib.js";
+
+export function renderUserBlock(
+  userName = "Wade Warren",
+  userAvatar = "/img/avatar.png",
+  favoriteItemsAmount
+) {
+  const favoritesCaption =
+    favoriteItemsAmount < 1 ? "ничего нет" : favoriteItemsAmount;
+  const hasFavoriteItems = favoriteItemsAmount >= 1 ? true : false;
+  const name = userName;
+  const avatar = userAvatar;
+
+  renderBlock(
+    "user-block",
+    `
+    <div class="header-container">
+      <img class="avatar" src="${avatar}" alt="Wade Warren" />
+      <div class="info">
+          <p class="name">${name}</p>
+          <p class="fav">
+            <i class="heart-icon${
+              hasFavoriteItems ? " active" : ""
+            }"></i>${favoritesCaption}
+          </p>
+      </div>
+    </div>
+    `
+  );
+}
